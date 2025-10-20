ca47b1ccc4eca3830d925652322b53fcb16d27cb
Nguyen Tuan Vu <tuanvunguyen3333@example.com>
2025-10-20 19:32:05 +0700

    Update AppServiceProvider: add caching for categories

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,32 +1,61 @@
 <?php
 
+
+
 namespace App\Providers;
 
+
+
 use Illuminate\Support\ServiceProvider;
+
 use Illuminate\Support\Facades\View;
+
 use App\Models\Category;
+
 use Illuminate\Support\Facades\Cache;
 
+
+
 class AppServiceProvider extends ServiceProvider
+
 {
-    /**
-     * Register any application services.
-     */
-    public function register(): void
-    {
-        //
-    }
 
     /**
-     * Bootstrap any application services.
+
+     * Register any application services.
+
      */
-    public function boot(): void
+
+    public function register(): void
+
     {
+
+        //
+
+    }
+
+
+
+    /**
+
+     * Bootstrap any application services.
+
+     */
+
+    public function boot(): void
+
+    {
+
         // Share biến $categories cho tất cả view với caching
+
         View::composer('*', function ($view) {
+
             $categories = Cache::remember('categories', 3600, function () { // Cache 1 giờ
+
                 return Category::orderBy('catename', 'asc')->get();
             });
+
+
 
             $view->with('categories', $categories);
         });
