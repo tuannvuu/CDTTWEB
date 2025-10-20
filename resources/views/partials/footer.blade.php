ec737947eccd9f65de15088833ca78d2024b952c
Nguyen Tuan Vu <tuanvunguyen3333@example.com>
2025-10-20 20:12:39 +0700

    Fix CSS load and HTTPS issue

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -7,7 +7,7 @@
     <title>Footer Chuyên Nghiệp - TechZone</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.0/font/bootstrap-icons.css">
-     <link href="{{ asset('css/footer.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/footer.css') }}">
 </head>
 
 <body>
