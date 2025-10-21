2dbe5fff7b76c0882d0ceaa83976c90ce2a2e8b7
Nguyen Tuan Vu <tuanvunguyen3333@example.com>
2025-10-21 09:19:06 +0700

    Cập nhật code mới

--- resources/views/partials/headerprodetails.blade.php
+++ resources/views/partials/headerprodetails.blade.php
@@ -9,7 +9,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <link href="{{ asset('css/header.css') }}" rel="stylesheet">
 
 
 
